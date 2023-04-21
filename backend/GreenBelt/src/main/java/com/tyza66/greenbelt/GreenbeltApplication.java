11568f789f19735533495bf9d9e31037ac3225a1
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-21 09:24:59 +0800

    update

---
+++ backend/GreenBelt/src/main/java/com/tyza66/greenbelt/GreenbeltApplication.java
@@ -0,0 +1,13 @@
+package com.tyza66.greenbelt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenbeltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenbeltApplication.class, args);
+    }
+
+}
---
+++ backend/GreenBelt/src/test/java/com/tyza66/greenbelt/GreenbeltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyza66.greenbelt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenbeltApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
