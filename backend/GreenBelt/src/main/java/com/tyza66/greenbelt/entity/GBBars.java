14b28ed01ac79f5a82b8c89c5bba62c10806e3e0
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-03 15:43:54 +0800

    update

---
+++ backend/GreenBelt/src/main/java/com/tyza66/greenbelt/entity/GBBars.java
@@ -0,0 +1,16 @@
+package com.tyza66.greenbelt.entity;
+
+import lombok.Data;
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/03 15:42
+ * Github: https://github.com/tyza66
+ **/
+@Data
+public class GBBars {
+    int id;
+    String address;
+    String name;
+    int group;
+}
