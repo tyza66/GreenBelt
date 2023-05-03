110a4da4e37ca160fe363fe0a6eb60a5d3f8fa95
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-03 14:21:53 +0800

    update

--- frontend/vuecli-greenbelt/src/router/index.js
+++ frontend/vuecli-greenbelt/src/router/index.js
@@ -10,10 +10,11 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },{
+    path: '/help',
+    name: 'help',
+    component: () => import('../views/HelpView.vue')
   }
 ]
 
--- frontend/vuecli-greenbelt/src/views/AboutView.vue
+++ frontend/vuecli-greenbelt/src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>https://github.com/tyza66/GreenBelt</h1>
+    <h1>Green Belt</h1>
+    <h1>基于大数据和物联网的可视化城市绿化带自动灌溉系统</h1>
+    <h1>开源地址：https://github.com/tyza66/GreenBelt</h1>
   </div>
 </template>
