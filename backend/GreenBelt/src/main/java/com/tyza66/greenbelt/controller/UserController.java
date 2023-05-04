0b590bb5c757e17fb7629c001a83c61245d1b98d
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-04 14:16:39 +0800

    update

--- backend/GreenBelt/src/main/java/com/tyza66/greenbelt/controller/UserController.java
+++ backend/GreenBelt/src/main/java/com/tyza66/greenbelt/controller/UserController.java
@@ -3,7 +3,6 @@ package com.tyza66.greenbelt.controller;
 import cn.hutool.json.JSON;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-import com.sun.org.apache.bcel.internal.generic.NEW;
 import com.tyza66.greenbelt.entity.User;
 import com.tyza66.greenbelt.mapper.UserMapper;
 import io.swagger.annotations.Api;
@@ -11,9 +10,17 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Author: tyza66
@@ -25,6 +32,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private String number;
     @Resource
     UserMapper userMapper;
 
@@ -54,5 +62,59 @@ public class UserController {
             toJson.set("status", "-1");
         }
         return toJson;
+    }
+
+    @ApiOperation(value = "生产图片验证码")
+    @GetMapping("/qcr")
+    public void getQCR(HttpServletResponse response) throws IOException {
+        //创建空白图片
+        BufferedImage image = new BufferedImage(100, 30, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        Random r = new Random();
+        //设置画笔颜色
+        g.setColor(new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+        //绘制矩形背景
+        g.fillRect(0, 0, 100, 30);
+        //绘制八条干扰线
+        for (int i = 1; i <= 8; i++) {
+            g.setColor(new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+            g.drawLine(r.nextInt(100), r.nextInt(30), r.nextInt(100), r.nextInt(30));
+        }
+        //画字母
+        g.setColor(new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+        number = getNumbers(5);
+        g.setFont(new Font(null, Font.BOLD + Font.ITALIC, 24));
+        g.drawString(number, 5, 25);
+        response.setContentType("image/jpeg");
+        OutputStream out = response.getOutputStream();
+        ImageIO.write(image, "jpeg", out);
+        out.close();
+    }
+
+    //提供随机种子
+    public String getNumbers(int size) {
+        String str = "df5d6as2ekfofsaffp66sgfsg8a6fhoi635g4s56g4s65gg5464f4ewgs22d65g";
+        String number = "";
+        Random r = new Random();
+        for (int i = 0; i < size; i++) {
+            number = number + str.charAt(r.nextInt(str.length()));
+        }
+        return number;
+    }
+
+    @ApiOperation(value = "注册新用户")
+    @PostMapping("/sign")
+    public JSON sign(@RequestBody User user) {
+        JSONObject json = JSONUtil.createObj();
+        if (user.getNickname().equalsIgnoreCase(number)) {
+            if (userMapper.newUserCount(user) >= 1) {
+                json.set("statu", "ok");
+            } else {
+                json.set("statu", "sjk");
+            }
+        } else {
+            json.set("statu", "yzm");
+        }
+        return json;
     }
 }
