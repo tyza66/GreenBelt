964dc0083a010793e4f7c1bd548e474294858384
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-08 09:00:33 +0800

    update

--- frontend/vuecli-greenbelt/src/views/HomeView.vue
+++ frontend/vuecli-greenbelt/src/views/HomeView.vue
@@ -136,7 +136,9 @@
           </el-tab-pane>
           <el-tab-pane label="关于">
             <div class="gy">
-
+              <h1>Green Belt</h1>
+              <h1>基于大数据和物联网的可视化城市绿化带自动灌溉系统</h1>
+              <h1>开源地址：https://github.com/tyza66/GreenBelt</h1>
             </div>
           </el-tab-pane>
         </el-tabs>
