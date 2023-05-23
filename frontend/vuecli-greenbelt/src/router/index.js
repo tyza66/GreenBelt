dad032809351f4b17a310461de879c092264093c
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-23 15:53:32 +0800

    update

--- frontend/vuecli-greenbelt/src/router/index.js
+++ frontend/vuecli-greenbelt/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LogView from '../views/LogView.vue'
 
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
